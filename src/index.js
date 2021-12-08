063c3a8a24acae1b871f00e4f41ef5acf632d121
ArtemLytvynenkoa <chpart355@gmail.com>
2021-12-09 01:24:27 +0200

    adds all tasks

--- src/index.js
+++ src/index.js
@@ -2,4 +2,77 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-const axios = require('axios').default;
+import SearchImgService from './js/fetchImgService'
+import getRefs from './js/refs';
+import markupGallery from './templates/img-card.hbs' 
+
+
+const searchImgService = new SearchImgService();
+const refs = getRefs();
+const optionsLightBox = {
+    captions: true,
+    captionDelay: 250,
+};
+const lightbox  = new SimpleLightbox(".gallery a", optionsLightBox);
+
+refs.searchForm.addEventListener('submit', onSubmitSearch)
+refs.loadMoreBtn.addEventListener('click', onBtnClick)
+
+function onSubmitSearch(event) {
+    event.preventDefault();
+
+    const searchQuery = event.currentTarget.elements.searchQuery.value.replaceAll(' ', '+');
+
+    searchImgService.query = searchQuery;
+
+    remove()
+    render()
+}
+
+function galleryRender(imgArray) {
+    if (!imgArray) {
+        return
+    }  
+    refs.gallery.insertAdjacentHTML('beforeend', markupGallery(imgArray))
+}
+
+function onBtnClick() {
+    refs.loadMoreBtn.classList.add('visually-hidden');
+
+    render()
+}
+
+function smoothScroll() {
+    const { height: cardHeight } = document
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+    });
+}
+
+async function render() {
+    const imgArray = await searchImgService.fetchImg();
+
+    galleryRender(imgArray);
+
+    searchImgService.incrementNumberImg();
+    searchImgService.incrementPage();
+    lightbox.refresh();
+
+    if (!imgArray) {
+        return
+    }
+
+    refs.loadMoreBtn.classList.remove('visually-hidden');
+
+    smoothScroll();
+}
+
+function remove() {
+    searchImgService.resetNumberImg();
+    searchImgService.resetPage();
+    refs.gallery.innerHTML = ""
+}
